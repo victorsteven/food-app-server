037208a4f2774d2c347e539a136e4cefdfab1713
steven victor <chikodi543@gmail.com>
2020-02-07 13:40:59 +0100

    Include .env for heroku build

--- domain/repository/food_repository.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-import "food-app/domain/entity"
-
-type FoodRepository interface {
-	SaveFood(*entity.Food) (*entity.Food, error)
-	GetFood(uint64) (*entity.Food, error)
-	GetAllFood() ([]entity.Food, error)
-}
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"food-app/cmd"
-)
-
-func main() {
-	cmd.StartApp()
-}
