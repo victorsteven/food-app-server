3675054b2dac71f035d339a1c6bb03befac2409b
steven victor <chikodi543@gmail.com>
2020-02-19 03:10:22 +0100

    refactor user's app

--- application/user_app.go
+++ application/user_app.go
@@ -1,7 +1,6 @@
 package application
 
 import (
-	"food-app/database/rdbms"
 	"food-app/domain/entity"
 	"food-app/domain/infrastructure"
 )
@@ -20,31 +19,18 @@ type UserAppInterface interface {
 
 
 func (u *userApp) SaveUser(user *entity.User) (*entity.User, map[string]string) {
-	//db := rdbms.NewDB()
-	//conn := infrastructure.NewUserRepository(db)
-	//return conn.SaveUser(user)
 	return infrastructure.UserRepo.SaveUser(user)
 }
 
 func (u *userApp) GetUser(userId uint64) (*entity.User, error) {
-	db := rdbms.NewDB()
-	conn := infrastructure.NewUserRepository(db)
-	return conn.GetUser(userId)
+	return infrastructure.UserRepo.GetUser(userId)
 }
 
 func (u *userApp) GetUsers() ([]entity.User, error) {
-	db := rdbms.NewDB()
-	conn := infrastructure.NewUserRepository(db)
-	return conn.GetUsers()
+	return infrastructure.UserRepo.GetUsers()
 }
 
 func (u *userApp) GetUserByEmailAndPassword(user *entity.User) (*entity.User, map[string]string) {
-	db := rdbms.NewDB()
-	conn := infrastructure.NewUserRepository(db)
-	foundUser, err := conn.GetUserByEmailAndPassword(user)
-	if err != nil {
-		return nil, err
-	}
-	return foundUser, nil
+	return infrastructure.UserRepo.GetUserByEmailAndPassword(user)
 }
 
