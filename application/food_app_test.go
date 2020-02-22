e524a136cb3ff2e2958323a36d007092b2027e34
steven victor <chikodi543@gmail.com>
2020-02-22 12:14:03 +0100

    Add more tests to the application layer

--- application/food_app_test.go
+++ application/food_app_test.go
@@ -7,36 +7,36 @@ import (
 	"testing"
 )
 
-//IF YOU HAVE TIME, YOU CAN TEST ALL THE METHODS
+//IF YOU HAVE TIME, YOU CAN TEST ALL THE METHODS FAILURES
 
 type fakeFoodRepo struct {}
 
 var (
 	saveFoodRepo func(*entity.Food) (*entity.Food, map[string]string)
+	getFoodRepo func(uint64) (*entity.Food, error)
+	getAllFoodRepo func() ([]entity.Food, error)
+	updateFoodRepo func(*entity.Food) (*entity.Food, map[string]string)
+	deleteFoodRepo func(uint64) error
 )
 
-func (f fakeFoodRepo) SaveFood(food *entity.Food) (*entity.Food, map[string]string) {
+func (f *fakeFoodRepo) SaveFood(food *entity.Food) (*entity.Food, map[string]string) {
 	return saveFoodRepo(food)
 }
-
-func (f fakeFoodRepo) GetFood(uint64) (*entity.Food, error) {
-	panic("implement me")
+func (f *fakeFoodRepo) GetFood(foodId uint64) (*entity.Food, error) {
+	return getFoodRepo(foodId)
 }
-
-func (f fakeFoodRepo) GetAllFood() ([]entity.Food, error) {
-	panic("implement me")
+func (f *fakeFoodRepo) GetAllFood() ([]entity.Food, error) {
+	return getAllFoodRepo()
 }
-
-func (f fakeFoodRepo) UpdateFood(*entity.Food) (*entity.Food, map[string]string) {
-	panic("implement me")
+func (f *fakeFoodRepo) UpdateFood(food *entity.Food) (*entity.Food, map[string]string) {
+	return updateFoodRepo(food)
 }
-
-func (f fakeFoodRepo) DeleteFood(uint64) error {
-	panic("implement me")
+func (f *fakeFoodRepo) DeleteFood(foodId uint64) error {
+	return deleteFoodRepo(foodId)
 }
 
 func TestSaveFood_Success(t *testing.T) {
-	infrastructure.FoodRepo = &fakeFoodRepo{}
+	infrastructure.FoodRepo = &fakeFoodRepo{}  //this is where the real implementation is swapped with the fake
 	//Mock the response coming from the infrastructure
 	saveFoodRepo = func(user *entity.Food) (*entity.Food,  map[string]string) {
 		return &entity.Food{
@@ -57,4 +57,83 @@ func TestSaveFood_Success(t *testing.T) {
 	assert.EqualValues(t, f.Title, "food title")
 	assert.EqualValues(t, f.Description, "food description")
 	assert.EqualValues(t, f.UserID, 1)
+}
+
+func TestGetFood_Success(t *testing.T) {
+	infrastructure.FoodRepo = &fakeFoodRepo{} //this is where the real implementation is swapped with the fake
+	//Mock the response coming from the infrastructure
+	getFoodRepo = func(foodId uint64) (*entity.Food,  error) {
+		return &entity.Food{
+			ID:        1,
+			Title: "food title",
+			Description:  "food description",
+			UserID:     1,
+		}, nil
+	}
+	foodId := uint64(1)
+	f, err := FoodApp.GetFood(foodId)
+	assert.Nil(t, err)
+	assert.EqualValues(t, f.Title, "food title")
+	assert.EqualValues(t, f.Description, "food description")
+	assert.EqualValues(t, f.UserID, 1)
+}
+
+func TestAllFood_Success(t *testing.T) {
+	infrastructure.FoodRepo = &fakeFoodRepo{} //this is where the real implementation is swapped with the fake
+	//Mock the response coming from the infrastructure
+	getAllFoodRepo = func() ([]entity.Food,  error) {
+		return []entity.Food{
+			{
+				ID:        1,
+				Title: "food title first",
+				Description:  "food description first",
+				UserID:     1,
+			},
+			{
+				ID:        2,
+				Title: "food title second",
+				Description:  "food description second",
+				UserID:     1,
+			},
+
+		}, nil
+	}
+	f, err := FoodApp.GetAllFood()
+	assert.Nil(t, err)
+	assert.EqualValues(t, len(f), 2)
+}
+
+func TestUpdateFood_Success(t *testing.T) {
+	infrastructure.FoodRepo = &fakeFoodRepo{} //this is where the real implementation is swapped with the fake
+	//Mock the response coming from the infrastructure
+	updateFoodRepo = func(user *entity.Food) (*entity.Food,  map[string]string) {
+		return &entity.Food{
+			ID:        1,
+			Title: "food title update",
+			Description:  "food description update",
+			UserID:     1,
+		}, nil
+	}
+	food := &entity.Food{
+		ID:        1,
+		Title: "food title update",
+		Description:  "food description update",
+		UserID:     1,
+	}
+	f, err := FoodApp.UpdateFood(food)
+	assert.Nil(t, err)
+	assert.EqualValues(t, f.Title, "food title update")
+	assert.EqualValues(t, f.Description, "food description update")
+	assert.EqualValues(t, f.UserID, 1)
+}
+
+func TestDeleteFood_Success(t *testing.T) {
+	infrastructure.FoodRepo = &fakeFoodRepo{} //this is where the real implementation is swapped with the fake
+	//Mock the response coming from the infrastructure
+	deleteFoodRepo = func(foodId uint64) error {
+		return nil
+	}
+	foodId := uint64(1)
+	err := FoodApp.DeleteFood(foodId)
+	assert.Nil(t, err)
 }
--- application/user_app_test.go
+++ application/user_app_test.go
@@ -7,10 +7,13 @@ import (
 	"testing"
 )
 
-//IF YOU HAVE TIME, YOU CAN TEST ALL THE METHODS
+//IF YOU HAVE TIME, YOU CAN TEST ALL THE METHODS FAILURES
 
 var (
 	saveUserRepo func(*entity.User) (*entity.User, map[string]string)
+	getUserRepo func(userId uint64) (*entity.User, error)
+	getUsersRepo func() ([]entity.User, error)
+	getUserEmailAndPasswordRepo func(*entity.User) (*entity.User, map[string]string)
 )
 
 type fakeUserRepo struct {}
@@ -18,21 +21,18 @@ type fakeUserRepo struct {}
 func (u *fakeUserRepo) SaveUser(user *entity.User) (*entity.User, map[string]string) {
 	return saveUserRepo(user)
 }
-
-func (u *fakeUserRepo) GetUser(uint64) (*entity.User, error) {
-	panic("implement me")
+func (u *fakeUserRepo) GetUser(userId uint64) (*entity.User, error) {
+	return getUserRepo(userId)
 }
-
 func (u *fakeUserRepo) GetUsers() ([]entity.User, error) {
-	panic("implement me")
+	return getUsersRepo()
 }
-
-func (u *fakeUserRepo) GetUserByEmailAndPassword(*entity.User) (*entity.User, map[string]string) {
-	panic("implement me")
+func (u *fakeUserRepo) GetUserByEmailAndPassword(user *entity.User) (*entity.User, map[string]string) {
+	return getUserEmailAndPasswordRepo(user)
 }
 
 func TestSaveUser_Success(t *testing.T) {
-	infrastructure.UserRepo = &fakeUserRepo{}
+	infrastructure.UserRepo = &fakeUserRepo{} //swap the real method with the fake
 	//Mock the response coming from the infrastructure
 	saveUserRepo = func(user *entity.User) (*entity.User,  map[string]string) {
 		return &entity.User{
@@ -51,6 +51,78 @@ func TestSaveUser_Success(t *testing.T) {
 		Password:  "password",
 	}
 	u, err := UserApp.SaveUser(user)
+	assert.Nil(t, err)
+	assert.EqualValues(t, u.FirstName, "victor")
+	assert.EqualValues(t, u.LastName, "steven")
+	assert.EqualValues(t, u.Email, "steven@example.com")
+}
+
+func TestGetUser_Success(t *testing.T) {
+	infrastructure.UserRepo = &fakeUserRepo{} //swap the real method with the fake
+	//Mock the response coming from the infrastructure
+	getUserRepo = func(userId uint64) (*entity.User,  error) {
+		return &entity.User{
+			ID:        1,
+			FirstName: "victor",
+			LastName:  "steven",
+			Email:     "steven@example.com",
+			Password:  "password",
+		}, nil
+	}
+	userId := uint64(1)
+	u, err := UserApp.GetUser(userId)
+	assert.Nil(t, err)
+	assert.EqualValues(t, u.FirstName, "victor")
+	assert.EqualValues(t, u.LastName, "steven")
+	assert.EqualValues(t, u.Email, "steven@example.com")
+}
+
+func TestGetUsers_Success(t *testing.T) {
+	infrastructure.UserRepo = &fakeUserRepo{} //swap the real method with the fake
+	//Mock the response coming from the infrastructure
+	getUsersRepo = func() ([]entity.User, error) {
+		return []entity.User{
+			{
+				ID:        1,
+				FirstName: "victor",
+				LastName:  "steven",
+				Email:     "steven@example.com",
+				Password:  "password",
+			},
+			{
+				ID:        2,
+				FirstName: "kobe",
+				LastName:  "bryant",
+				Email:     "kobe@example.com",
+				Password:  "password",
+			},
+		}, nil
+	}
+	users, err := UserApp.GetUsers()
+	assert.Nil(t, err)
+	assert.EqualValues(t, len(users), 2)
+}
+
+func TestGetUserByEmailAndPassword_Success(t *testing.T) {
+	infrastructure.UserRepo = &fakeUserRepo{} //swap the real method with the fake
+	//Mock the response coming from the infrastructure
+	getUserEmailAndPasswordRepo = func(user *entity.User) (*entity.User,  map[string]string) {
+		return &entity.User{
+			ID:        1,
+			FirstName: "victor",
+			LastName:  "steven",
+			Email:     "steven@example.com",
+			Password:  "password",
+		}, nil
+	}
+	user := &entity.User{
+		ID:        1,
+		FirstName: "victor",
+		LastName:  "steven",
+		Email:     "steven@example.com",
+		Password:  "password",
+	}
+	u, err := UserApp.GetUserByEmailAndPassword(user)
 	assert.Nil(t, err)
 	assert.EqualValues(t, u.FirstName, "victor")
 	assert.EqualValues(t, u.LastName, "steven")
