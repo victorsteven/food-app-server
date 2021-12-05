a9f3225b7a2ec52216c4c095f99dbf92146755a4
Rancho <rancho.z@binance.com>
2021-12-05 21:40:21 +0800

    fix error check order

--- infrastructure/persistence/food_repository.go
+++ infrastructure/persistence/food_repository.go
@@ -1,90 +1,91 @@
 package persistence
 
 import (
-	"errors"
-	"food-app/domain/entity"
-	"food-app/domain/repository"
-	"github.com/jinzhu/gorm"
-	"os"
-	"strings"
+    "errors"
+    "os"
+    "strings"
+
+    "github.com/jinzhu/gorm"
+
+    "food-app/domain/entity"
+    "food-app/domain/repository"
 )
 
-
 type FoodRepo struct {
-	db *gorm.DB
+    db *gorm.DB
 }
 
 func NewFoodRepository(db *gorm.DB) *FoodRepo {
-	return &FoodRepo{db}
+    return &FoodRepo{db}
 }
 
-//FoodRepo implements the repository.FoodRepository interface
+// FoodRepo implements the repository.FoodRepository interface
 var _ repository.FoodRepository = &FoodRepo{}
 
 func (r *FoodRepo) SaveFood(food *entity.Food) (*entity.Food, map[string]string) {
-	dbErr := map[string]string{}
-	//The images are uploaded to digital ocean spaces. So we need to prepend the url. This might not be your use case, if you are not uploading image to Digital Ocean.
-	food.FoodImage = os.Getenv("DO_SPACES_URL") + food.FoodImage
+    dbErr := map[string]string{}
+    // The images are uploaded to digital ocean spaces. So we need to prepend the url. This might not be your use case, if you are not uploading image to Digital Ocean.
+    food.FoodImage = os.Getenv("DO_SPACES_URL") + food.FoodImage
 
-	err := r.db.Debug().Create(&food).Error
-	if err != nil {
-		//since our title is unique
-		if strings.Contains(err.Error(), "duplicate") || strings.Contains(err.Error(), "Duplicate") {
-			dbErr["unique_title"] = "food title already taken"
-			return nil, dbErr
-		}
-		//any other db error
-		dbErr["db_error"] = "database error"
-		return nil, dbErr
-	}
-	return food, nil
+    err := r.db.Debug().Create(&food).Error
+    if err != nil {
+        // since our title is unique
+        if strings.Contains(err.Error(), "duplicate") || strings.Contains(err.Error(), "Duplicate") {
+            dbErr["unique_title"] = "food title already taken"
+            return nil, dbErr
+        }
+        // any other db error
+        dbErr["db_error"] = "database error"
+        return nil, dbErr
+    }
+    return food, nil
 }
 
 func (r *FoodRepo) GetFood(id uint64) (*entity.Food, error) {
-	var food entity.Food
-	err := r.db.Debug().Where("id = ?", id).Take(&food).Error
-	if err != nil {
-		return nil, errors.New("database error, please try again")
-	}
-	if gorm.IsRecordNotFoundError(err) {
-		return nil, errors.New("food not found")
-	}
-	return &food, nil
+    var food entity.Food
+    err := r.db.Debug().Where("id = ?", id).Take(&food).Error
+    if gorm.IsRecordNotFoundError(err) {
+        return nil, errors.New("food not found")
+    }
+    if err != nil {
+        return nil, errors.New("database error, please try again")
+    }
+    return &food, nil
 }
 
 func (r *FoodRepo) GetAllFood() ([]entity.Food, error) {
-	var foods []entity.Food
-	err := r.db.Debug().Limit(100).Order("created_at desc").Find(&foods).Error
-	if err != nil {
-		return nil, err
-	}
-	if gorm.IsRecordNotFoundError(err) {
-		return nil, errors.New("user not found")
-	}
-	return foods, nil
+    var foods []entity.Food
+    err := r.db.Debug().Limit(100).Order("created_at desc").Find(&foods).Error
+    if gorm.IsRecordNotFoundError(err) {
+        return nil, errors.New("user not found")
+    }
+    if err != nil {
+        return nil, err
+    }
+    return foods, nil
 }
 
 func (r *FoodRepo) UpdateFood(food *entity.Food) (*entity.Food, map[string]string) {
-	dbErr := map[string]string{}
-	err := r.db.Debug().Save(&food).Error
-	if err != nil {
-		//since our title is unique
-		if strings.Contains(err.Error(), "duplicate") || strings.Contains(err.Error(), "Duplicate") {
-			dbErr["unique_title"] = "title already taken"
-			return nil, dbErr
-		}
-		//any other db error
-		dbErr["db_error"] = "database error"
-		return nil, dbErr
-	}
-	return food, nil
+    dbErr := map[string]string{}
+    err := r.db.Debug().Save(&food).Error
+    if err != nil {
+        // since our title is unique
+        if strings.Contains(err.Error(), "duplicate") || strings.Contains(err.Error(), "Duplicate") {
+            dbErr["unique_title"] = "title already taken"
+            return nil, dbErr
+        }
+        // any other db error
+        dbErr["db_error"] = "database error"
+        return nil, dbErr
+    }
+    return food, nil
 }
 
 func (r *FoodRepo) DeleteFood(id uint64) error {
-	var food entity.Food
-	err := r.db.Debug().Where("id = ?", id).Delete(&food).Error
-	if err != nil {
-		return errors.New("database error, please try again")
-	}
-	return nil
+    var food entity.Food
+    err := r.db.Debug().Where("id = ?", id).Delete(&food).Error
+    if err != nil {
+        return errors.New("database error, please try again")
+    }
+    return nil
 }
--- infrastructure/persistence/user_repository.go
+++ infrastructure/persistence/user_repository.go
@@ -1,82 +1,85 @@
 package persistence
 
 import (
-	"errors"
-	"food-app/domain/entity"
-	"food-app/domain/repository"
-	"food-app/infrastructure/security"
-	"github.com/jinzhu/gorm"
-	"golang.org/x/crypto/bcrypt"
-	"strings"
+    "errors"
+    "strings"
+
+    "github.com/jinzhu/gorm"
+    "golang.org/x/crypto/bcrypt"
+
+    "food-app/domain/entity"
+    "food-app/domain/repository"
+    "food-app/infrastructure/security"
 )
 
 type UserRepo struct {
-	db *gorm.DB
+    db *gorm.DB
 }
 
 func NewUserRepository(db *gorm.DB) *UserRepo {
-	return &UserRepo{db}
+    return &UserRepo{db}
 }
-//UserRepo implements the repository.UserRepository interface
+
+// UserRepo implements the repository.UserRepository interface
 var _ repository.UserRepository = &UserRepo{}
 
 func (r *UserRepo) SaveUser(user *entity.User) (*entity.User, map[string]string) {
-	dbErr := map[string]string{}
-	err := r.db.Debug().Create(&user).Error
-	if err != nil {
-		//If the email is already taken
-		if strings.Contains(err.Error(), "duplicate") || strings.Contains(err.Error(), "Duplicate") {
-			dbErr["email_taken"] = "email already taken"
-			return nil, dbErr
-		}
-		//any other db error
-		dbErr["db_error"] = "database error"
-		return nil, dbErr
-	}
-	return user, nil
+    dbErr := map[string]string{}
+    err := r.db.Debug().Create(&user).Error
+    if err != nil {
+        // If the email is already taken
+        if strings.Contains(err.Error(), "duplicate") || strings.Contains(err.Error(), "Duplicate") {
+            dbErr["email_taken"] = "email already taken"
+            return nil, dbErr
+        }
+        // any other db error
+        dbErr["db_error"] = "database error"
+        return nil, dbErr
+    }
+    return user, nil
 }
 
 func (r *UserRepo) GetUser(id uint64) (*entity.User, error) {
-	var user entity.User
-	err := r.db.Debug().Where("id = ?", id).Take(&user).Error
-	if err != nil {
-		return nil, err
-	}
-	if gorm.IsRecordNotFoundError(err) {
-		return nil, errors.New("user not found")
-	}
-	return &user, nil
+    var user entity.User
+    err := r.db.Debug().Where("id = ?", id).Take(&user).Error
+    if gorm.IsRecordNotFoundError(err) {
+        return nil, errors.New("user not found")
+    }
+    if err != nil {
+        return nil, err
+    }
+    return &user, nil
 }
 
 func (r *UserRepo) GetUsers() ([]entity.User, error) {
-	var users []entity.User
-	err := r.db.Debug().Find(&users).Error
-	if err != nil {
-		return nil, err
-	}
-	if gorm.IsRecordNotFoundError(err) {
-		return nil, errors.New("user not found")
-	}
-	return users, nil
+    var users []entity.User
+    err := r.db.Debug().Find(&users).Error
+    if gorm.IsRecordNotFoundError(err) {
+        return nil, errors.New("user not found")
+    }
+    if err != nil {
+        return nil, err
+    }
+    return users, nil
 }
 
 func (r *UserRepo) GetUserByEmailAndPassword(u *entity.User) (*entity.User, map[string]string) {
-	var user entity.User
-	dbErr := map[string]string{}
-	err := r.db.Debug().Where("email = ?", u.Email).Take(&user).Error
-	if gorm.IsRecordNotFoundError(err) {
-		dbErr["no_user"] = "user not found"
-		return nil, dbErr
-	}
-	if err != nil {
-		dbErr["db_error"] = "database error"
-		return nil, dbErr
-	}
-	//Verify the password
-	err = security.VerifyPassword(user.Password, u.Password)
-	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
-		dbErr["incorrect_password"] = "incorrect password"
-		return nil, dbErr
-	}
-	return &user, nil
+    var user entity.User
+    dbErr := map[string]string{}
+    err := r.db.Debug().Where("email = ?", u.Email).Take(&user).Error
+    if gorm.IsRecordNotFoundError(err) {
+        dbErr["no_user"] = "user not found"
+        return nil, dbErr
+    }
+    if err != nil {
+        dbErr["db_error"] = "database error"
+        return nil, dbErr
+    }
+    // Verify the password
+    err = security.VerifyPassword(user.Password, u.Password)
+    if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+        dbErr["incorrect_password"] = "incorrect password"
+        return nil, dbErr
+    }
+    return &user, nil
 }
